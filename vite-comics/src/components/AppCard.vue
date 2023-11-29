2ded3600b0de08e1dc3e961c4c4fca0db4452f33
Ilaria Salvati <ilariasalvati00@gmail.com>
2023-11-29 22:26:53 +0100

    aggiunto componente AppCard

---
+++ vite-comics/src/components/AppCard.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+    thumb: String,
+    title: String
+})
+</script>
+
+<template>
+    <div>
+        <div class="immagine" :style="{ 'background-image': 'url(' + thumb + ')' }"></div>
+        <p>{{ title }}</p>
+    </div>
+</template>
+
+<style scoped>
+div {
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+}
+
+.immagine {
+    width: 200px;
+    height: 200px;
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+
+p {
+    color: white;
+}
+</style>
